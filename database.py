7b540ccf5e390b364a40ca9655b02cb7441f96a3
Alwin <alwinng420@gmail.com>
2026-07-09 15:07:44 -0400

    basic database functionality, able to add fanouts to the database

--- database.py
+++ database.py
@@ -5,3 +5,23 @@ Tasks:
     - create fingerprints
     - add to database dictionary
 """
+
+import pickle
+
+def load() -> dict:
+    with open("db.pkl", mode = "rb") as opened_file:
+        return pickle.load(opened_file)
+
+def export(d: dict):   
+    with open("db.pkl",mode="wb") as opened_file:
+        pickle.dump(d,opened_file)
+
+def add(fanout: list[tuple[tuple[float,float,float],float]], song: str):
+    d = load()
+    for pair in fanout:
+        if pair[0] not in d:
+            d[pair[0]] = []
+        d[pair[0]].append((song, pair[1]))
+    export(d)
+
+
