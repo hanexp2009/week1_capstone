626148baab848a66e914842ae57c300f29e64fad
BagelBreaker <100354616+BagelBreaker@users.noreply.github.com>
2026-07-09 15:49:34 -0500

    spectrogram function added

--- spectrogram.py
+++ spectrogram.py
@@ -2,22 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from song_input import mic_input
 
-recorded_audio = mic_input(5)
-sampling_rate = 44100
-fig, ax = plt.subplots()
-S, freqs, times, im = ax.specgram(
-    recorded_audio,
-    NFFT=4096,
-    Fs=sampling_rate,
-    window=mlab.window_hanning,
-    noverlap=4096 // 2,
-    mode='magnitude',
-    scale="dB"
-)
-fig.colorbar(im)
+def make_spectrogram(samples, sampling_rate=44100):
+    S, freqs, times = mlab.specgram(
+        samples,
+        NFFT=4096,
+        Fs=sampling_rate,
+        window=mlab.window_hanning,
+        noverlap=4096 // 2,
+        mode='magnitude',
+    )
+    return S, freqs, times
 
-ax.set_xlabel("Time [seconds]")
-ax.set_ylabel("Frequency (Hz)")
-ax.set_title("Spectrogram of Recording")
-ax.set_ylim(0, 4000);
-plt.show()
+
